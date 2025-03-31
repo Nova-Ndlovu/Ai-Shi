717aff6df41833218b03850a27b214dca5b5578b
Phi Ndlovu <ndlovusiphilangenkosi@gmail.com>
2025-04-01 01:30:56 +0200

    Added the Typewriter effect
    Used a Typewriter plugin to make the poem display appear as though written by a typewriter.

--- src/script.js
+++ src/script.js
@@ -1,12 +1,23 @@
 function displayPoetry(response) {
   let poemElement = document.querySelector("#poem-text");
   poemElement.innerHTML = response.data.answer;
+
+  new Typewriter("#poem-text", {
+    strings: `<div>The sun rises</div>
+          <div>The moon sets</div>
+          <div>Your neck breaks</div>
+          <div>Staring down</div>
+          <div>at your screen.</div>`,
+    autoStart: true,
+    delay: 2,
+    cursor: "",
+  });
 }
 
 function generatePoetry(prompt) {
   let apiKey = "2903bd515o84e51d2d0at09a3a53f67a";
   let context =
-    "Please respond in the style of a stoic person who has lived their entire lives in the frigid northern hinterlands and values the warmth of companionship.";
+    "Please respond in the style of a stoic person who has lived their entire lives in the frigid northern hinterlands and values the warmth of companionship. Please also phrase the response in a poem.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoetry);
