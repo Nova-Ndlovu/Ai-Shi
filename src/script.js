0a2966883021c0944724cbb87d61c02ebf588690
Phi Ndlovu <ndlovusiphilangenkosi@gmail.com>
2025-04-22 02:53:10 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function displayPoetry(response) {
 function generatePoetry(prompt) {
   let apiKey = "2903bd515o84e51d2d0at09a3a53f67a";
   let context =
-    "You are tasked with creating a poem that does not exceed 12 lines and being particular about the poem's lineation by always begining each new verse on a new line whenever there is punctuation to create pause, using basic HTML and separating each line with a <br />. Please respond in the voice of a stoic person who has lived their entire lives in the frigid northern hinterlands and values the warmth of companionship, the awe inspiring beauty of nature and refuses to use more than a dozen verses when it comes to their poetry.";
+    "You are tasked with creating a poem that does not exceed 12 lines and being particular about the poem's lineation by always begining each new verse on a new line whenever there is punctuation to create pause, separating each line with a <br /> and using basic HTML. Please respond in the voice of a stoic person who has lived their entire life in the frigid northern hinterlands and values the warmth of companionship, the awe inspiring beauty of nature and refuses to use more than a dozen verses when it comes to their poetry.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let iSeeYou = document.querySelector("#poem-text");
